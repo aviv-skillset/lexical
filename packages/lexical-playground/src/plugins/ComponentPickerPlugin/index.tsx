017724aaf482a1d00f8e6d5249d15e88bb51fa23
Aviv Nakar <aviv@skillsetech.com>
2023-05-21 18:33:32 +0300

    remove excalidraw feature

--- packages/lexical-playground/src/plugins/ComponentPickerPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/ComponentPickerPlugin/index.tsx
@@ -29,7 +29,6 @@ import catTypingGif from '../../images/cat-typing.gif';
 import { EmbedConfigs } from '../AutoEmbedPlugin';
 import { INSERT_COLLAPSIBLE_COMMAND } from '../CollapsiblePlugin';
 import { InsertEquationDialog } from '../EquationsPlugin';
-import { INSERT_EXCALIDRAW_COMMAND } from '../ExcalidrawPlugin';
 import { INSERT_IMAGE_COMMAND, InsertImageDialog } from '../ImagesPlugin';
 import { InsertPollDialog } from '../PollPlugin';
 import { InsertNewTableDialog, InsertTableDialog } from '../TablePlugin';
@@ -245,11 +244,6 @@ export default function ComponentPickerMenuPlugin(): JSX.Element {
         icon: <i className="icon horizontal-rule" />,
         keywords: ['horizontal rule', 'divider', 'hr'],
         onSelect: () => editor.dispatchCommand(INSERT_HORIZONTAL_RULE_COMMAND, undefined),
-      }),
-      new ComponentPickerOption('Excalidraw', {
-        icon: <i className="icon diagram-2" />,
-        keywords: ['excalidraw', 'diagram', 'drawing'],
-        onSelect: () => editor.dispatchCommand(INSERT_EXCALIDRAW_COMMAND, undefined),
       }),
       new ComponentPickerOption('Poll', {
         icon: <i className="icon poll" />,
